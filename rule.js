2a817bbe25f1137a37fbad584f3b7049a761b6f7
jim_j_wu <jim_j_wu@trend.com.tw>
2018-04-09 00:03:48 +0800

    Initial release

---
+++ rule.js
@@ -0,0 +1,21 @@
+
+[
+
+{"rulename":"titlerule", "tag":{"min":1,"name":"head", "tag":{"min":1,"name":"title", "text": "true"}}},
+{"rulename":"metarule","tag":{"min":1,"name":"head", "tag":{"min":1,"name":"meta", "attribs": [{"name":"keywords"}]}}},
+{"rulename":"metarule","tag":{"min":1,"name":"head", "tag":{"min":1,"name":"meta", "attribs": [{"name":"descriptions"}]}}},
+{"rulename":"strongrule","tag":{"name":"strong", "min":15}},
+{"rulename":"hrule","tag":{"name":"H1", "min":1}},
+{"rulename":"imgrule","tag":{"min":1,"name":"img", "attribs":[{"alt":""}]}},
+{"rulename":"arule","tag":{"min":1,"name":"a", "attribs":[{"rel":""}]}}
+
+
+
+
+]
+
+
+
+
+
+
